36b4fbb845756c323559aa7f646e7948c8634d02
kerssiafonseca <kerssiacaroline02@gmail.com>
2023-08-04 20:32:59 -0300

    fix [exercicio_09]: ajuste no fuso horário do relógio

--- exercicio_09.py
+++ exercicio_09.py
@@ -3,17 +3,23 @@ import time
 
 def exibir_hora_atual():
     while True:
-        # Obter a hora atual
+        # Obter o objeto de hora atual
         hora_atual = datetime.datetime.now()
         
+        # Definir o fuso horário da máquina
+        fuso_horario_maquina = hora_atual.astimezone().tzinfo
+        
+        # Obter a hora atual no fuso horário da máquina
+        hora_atual_fuso_horario_maquina = hora_atual.astimezone(fuso_horario_maquina)
+        
         # Formatando a hora para exibição
-        hora_formatada = hora_atual.strftime("%H:%M:%S")
+        hora_formatada = hora_atual_fuso_horario_maquina.strftime("%H:%M:%S")
         
         # Limpar a tela para mostrar a hora atualizada
         print("\033c", end="")
         
         # Exibir a hora atual
-        print(f"Hora atual: {hora_formatada}", end="", flush=True)
+        print(f"Hora atual: ({fuso_horario_maquina}): {hora_formatada}", end="", flush=True)
         
         # Aguardar um segundo antes de atualizar novamente
         time.sleep(1)
